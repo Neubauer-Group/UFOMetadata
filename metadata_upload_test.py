8e031876646e8dc0b6ba0bd65bcaf8e5ca71f9d5
ZijunWang <101375426+ThanosWang@users.noreply.github.com>
2022-08-12 09:06:21 -0500

    Update metadata_upload_test.py

--- metadata_upload_test.py
+++ metadata_upload_test.py
@@ -9,7 +9,6 @@ else:
     fullpath = sys.argv[1]
     newmetadata = fullpath.split('/')[-1]
     path = os.path.dirname(os.path.realpath(fullpath))
-    print(path)
     os.chdir(path)
     filelist = os.listdir()
 
